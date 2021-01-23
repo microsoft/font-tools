37ba28eebeca85a4aadc88f8d3847ca4f3eff18e
Andrew Glass <asg@uw.edu>
2021-01-23 09:56:34 -0800

    Initialize V2Proto branch

--- EgyptianOpenType/config.py
+++ EgyptianOpenType/config.py
@@ -8,8 +8,8 @@ pvar = {
     'langsystag' : "dflt",
     'reffontname' : "Segoe UI Historic", # Used in test file to show reference form relative to current version
     'fontfilename' : "Egyptian Text", # Font name
-    'fontsrc' : "../../fonts/et/egyptiantext-COLR.ttf", # Path to source font
-    'fontout' : "out/egyptiantext.ttf", # Path to write output font
+    'fontsrc' : "../../fonts/et/egyptiantextV2Proto-COLR.ttf", # Path to source font
+    'fontout' : "out/egyptiantextV2Proto.ttf", # Path to write output font
     'hfu' : 314, # horizontal, font units per hieroglyph unit (1372 per 6 hieroglyph units)
     'hhu' : 8, # horizontal, hieroglyph units
     'chu' : 6, # composite horizontal units (i.e., excludes wide non-composing widths)
@@ -41,9 +41,9 @@ pvar = {
     'controlcodes' : [[':',';','v'],['*','.'],[],[],[],[],['+','='],['(','['],[')',']']],
     'useproxycontrols': 0, # use proxy code points for controls
     'proxycontrols' : [78691,78692,78693,78694,78695,78696,78697,78698,78699],
-    'cartouche' : 1,
+    'cartouche' : 0,
     'cartouchecodes' : ['cb','cfb','hwtb','hwttb','hwtbb','hwtfb','ce','cre','cfe','hwte','hwtte','hwtbe','hwtfe'],
-    'fortified' : 1,
+    'fortified' : 0,
     'mirror' : 1,
     # 'test' : {'font':1,'gdef':1,'groups':1,'haln':1,'pres':1,'abvs':0,'blws':0,'rlig':1,'psts':1,'rtlm':1,'vrt2':1,'gpos':1,'langsys':1,'anchors':1,'coda':1}
     'test' : {'font':0,'gdef':0,'groups':0,'haln':1,'pres':0,'abvs':0,'blws':0,'rlig':0,'psts':0,'rtlm':1,'vrt2':1,'gpos':0,'langsys':0,'anchors':0,'coda':0}
