99773c5c041b9d4014cc8925613511be81c9423c
Andrew Glass <asg@uw.edu>
2021-02-01 09:31:40 -0800

    Add arg support to eotgen

--- EgyptianOpenType/eotgen.py
+++ EgyptianOpenType/eotgen.py
@@ -3,8 +3,19 @@
 
 import os
 import sys
-from config import pvar
+from os import path
 from eotHelper import EotHelper
+d = True
+
+if len(sys.argv) > 1:
+    fontconfig = 'config_'+str(sys.argv[1])
+    if path.exists(fontconfig+'.py'):
+        print('Using config: '+fontconfig)
+        exec('from '+fontconfig+' import pvar')
+        d = False
+if (d): 
+    print ('Using default config')
+    from config import pvar
 
 eothelper = EotHelper(pvar)
 eothelper.initializeVTP()
