2a82f5491c77f24c161d8e7863de7a21b0394d96
Andrew Glass <xadxura@live.com>
2025-05-09 20:28:20 -0700

    Disable params

--- EgyptianOpenType/eotgen.py
+++ EgyptianOpenType/eotgen.py
@@ -7,12 +7,12 @@ from os import path
 from eotHelper import EotHelper
 d = True
 
-if len(sys.argv) > 1:
-    fontconfig = 'config_'+str(sys.argv[1])
-    if path.exists(fontconfig+'.py'):
-        print('Using config: '+fontconfig)
-        exec('from '+fontconfig+' import pvar')
-        d = False
+# if len(sys.argv) > 1:
+#     fontconfig = 'config_'+str(sys.argv[1])
+#     if path.exists(fontconfig+'.py'):
+#         print('Using config: '+fontconfig)
+#         exec('from '+fontconfig+' import pvar')
+#         d = False
 if (d): 
     print ('Using default config')
     from config import pvar
